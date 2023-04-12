8fab40917fb9699195d59a8d19d6eb4e25f31640
Vadim Mazur <mazurva@gmail.com>
2023-04-12 17:43:24 +0600

    Update CKEditor.php

--- CKEditor.php
+++ CKEditor.php
@@ -2,12 +2,22 @@
 
 namespace sibds\widgets;
 
+use yii\helpers\ArrayHelper;
+
 class CKEditor extends \dosamigos\ckeditor\CKEditor
 {
     public $preset = 'full';
 
     public function init(){
-        $this->clientOptions = \mihaildev\elfinder\ElFinder::ckeditorOptions('elfinder',[/* Some CKEditor Options */]);
+        $this->clientOptions = ArrayHelper::merge([
+            'allowedContent' => true,
+            'protectedSource' => [
+                new \yii\web\JsExpression('/<i[^>]*><\/i>/g'),
+                new \yii\web\JsExpression('/<span[^>]*><\/span>/g')
+            ]
+        ], $this->clientOptions);
+        $this->clientOptions = ArrayHelper::merge($this->clientOptions,
+            \mihaildev\elfinder\ElFinder::ckeditorOptions('elfinder',[/* Some CKEditor Options */]));
         parent::init();
     }
 }
